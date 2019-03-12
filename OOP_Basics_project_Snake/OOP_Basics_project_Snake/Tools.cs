fd2528a5ff041fed3a28f29313bdc44fecaf90ba
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-12 15:08:40 +0600

    подправлены размеры игровой области

--- OOP_Basics_project_Snake/OOP_Basics_project_Snake/Tools.cs
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Tools.cs
@@ -7,10 +7,10 @@ namespace OOP_Basics_project_Snake
 {
     static class Tools
     {
-        const int GAME_AREA_WIDTH = 100;
+        const int GAME_AREA_WIDTH = 80;
         const int GAME_AREA_HEIGHT = 20;
-        const int GAME_AREA_TOP = 2;
-        const int GAME_AREA_LEFT = 4;
+        const int GAME_AREA_TOP = 1;
+        const int GAME_AREA_LEFT = 0;
         const char GAME_AREA_CHAR = '#';
 
 
@@ -33,11 +33,17 @@ namespace OOP_Basics_project_Snake
 
             vLine = new VerticalLine(GAME_AREA_LEFT + GAME_AREA_WIDTH, GAME_AREA_TOP, GAME_AREA_TOP + GAME_AREA_HEIGHT, GAME_AREA_CHAR);
             vLine.Draw();
+
+            Console.SetWindowSize(GAME_AREA_LEFT + GAME_AREA_WIDTH + 2, GAME_AREA_TOP + GAME_AREA_HEIGHT + 3);
+            Console.SetBufferSize(GAME_AREA_LEFT + GAME_AREA_WIDTH + 2, GAME_AREA_TOP + GAME_AREA_HEIGHT + 3);
+
         }
 
         public static void WaitingEnter()
         {
             Console.SetCursorPosition(GAME_AREA_LEFT, GAME_AREA_TOP + GAME_AREA_HEIGHT + 1);
+            //Console.WriteLine("buffer: h=" + Console.BufferHeight.ToString() + " w=" + Console.BufferWidth.ToString());
+            //Console.WriteLine("window: h=" + Console.WindowHeight.ToString() + " w=" + Console.WindowWidth.ToString());
             Console.WriteLine("Press Enter to exit");
             Console.ReadLine();
         }
