3cc5e4c3aa92fb17f78078286293d2b502c98506
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-12 12:59:01 +0600

    создан класс Горизонтальная линия

---
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/HorizontalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Basics_project_Snake
+{
+    class HorizontalLine
+    {
+        private List<Point> pointList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symb)
+        {
+            pointList = new List<Point>();
+
+            for (int x = xLeft; x <= xRight; x++)
+                pointList.Add(new Point(x, y, symb));
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pointList)
+                p.Draw();
+        }
+    }
+}
