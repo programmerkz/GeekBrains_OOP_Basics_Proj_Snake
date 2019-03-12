ffc728b711cc1cb8847cf638ad9978f601e9cf51
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-12 15:07:44 +0600

    Создан класс Фигура

---
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Figure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Basics_project_Snake
+{
+    class Figure
+    {
+        protected List<Point> pointList;
+
+        public void Draw()
+        {
+            foreach (Point p in pointList)
+                p.Draw();
+        }
+    }
+}
