42c3e46e7bc51f85ac29fb1ff91c10b2c0cb7106
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-13 11:05:12 +0600

    добавлена возможность движения змейки (добавлены два метода)

--- OOP_Basics_project_Snake/OOP_Basics_project_Snake/Snake.cs
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Snake.cs
@@ -7,8 +7,11 @@ namespace OOP_Basics_project_Snake
 {
     class Snake:Figure
     {
+        public Direction moveDirection;
+
         public Snake(Point tail, int lenth, Direction direction)
         {
+            moveDirection = direction;
             pointList = new List<Point>();
 
             for (int i = 0; i < lenth; i++)
@@ -17,6 +20,37 @@ namespace OOP_Basics_project_Snake
                 bodyPoint.Move(i, direction);
                 pointList.Add(bodyPoint);
             }
+        }
+
+
+        internal void Move()
+        {
+            // удалить хвост (это первая точка в списке)
+            // получить из метода GetNextPoint() точку новой головы
+            // добавить эту точку в список
+            // слелать отрисовку (стереть хвост и нарисовать новую голову)
+
+            Point tailPoint = pointList.First();
+            pointList.Remove(tailPoint);
+
+            Point newHeadPoint = GetNextPoint();
+            pointList.Add(newHeadPoint);
+
+            tailPoint.Clear();
+            newHeadPoint.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            // получить точку Головы змейки (это последняя в списке)
+            // создать новую копию полученной точки
+            // сместить её по направлению движения змейки
+            // добавить в список точек
+
+            Point snakeHead = pointList.Last();
+            Point nextHeadPoint = new Point(snakeHead);
+            nextHeadPoint.Move(1, moveDirection);
+            return nextHeadPoint;
         }
     }
 }
