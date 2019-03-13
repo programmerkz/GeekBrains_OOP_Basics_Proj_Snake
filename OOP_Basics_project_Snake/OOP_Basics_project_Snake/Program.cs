dcdd506cf72bf75f9c40d725805fd468336d64a1
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-13 13:27:42 +0600

    переписан основной метод более логично и компактно в соответствии с новым функционалом

--- OOP_Basics_project_Snake/OOP_Basics_project_Snake/Program.cs
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Program.cs
@@ -11,15 +11,27 @@ namespace OOP_Basics_project_Snake
         static void Main(string[] args)
         {
             Tools.DrawGameArea(true);
-            Snake snake = new Snake(new Point(13, 14, '*'), 12, Direction.RIGHT);
+
+            Snake snake = new Snake(new Point(13, 14, Tools.SNAKE_CHAR), 3, Direction.RIGHT);
             snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(Tools.GAME_AREA_WIDTH, Tools.GAME_AREA_HEIGHT, Tools.GAME_AREA_TOP, Tools.GAME_AREA_LEFT, Tools.FOOD_CHAR);
+            Point food = foodCreator.CreateFood();
+            food.Draw();
 
             while (true)
             {
-                if (Console.KeyAvailable) { snake.HandleKey(Console.ReadKey().Key); }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                    snake.Move();
 
-                Thread.Sleep(100);
-                snake.Move();
+                Thread.Sleep(Tools.GAME_DELAY);
+
+                if (Console.KeyAvailable) { snake.HandleKey(Console.ReadKey().Key); }
             }
         }
     }
