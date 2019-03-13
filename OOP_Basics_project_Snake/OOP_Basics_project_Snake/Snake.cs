6b1f2ab0540e2f59195b6c4f4fb062b697065f4c
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-13 10:20:05 +0600

    Создан контруктор для класса Змейка

---
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Snake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Basics_project_Snake
+{
+    class Snake:Figure
+    {
+        public Snake(Point tail, int lenth, Direction direction)
+        {
+            pointList = new List<Point>();
+
+            for (int i = 0; i < lenth; i++)
+            {
+                Point bodyPoint = new Point(tail);
+                bodyPoint.Move(i, direction);
+                pointList.Add(bodyPoint);
+            }
+        }
+    }
+}
