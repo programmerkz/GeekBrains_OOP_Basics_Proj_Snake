fdd4c13a5fd809c0af154c661eee4c62d7fe8290
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-13 10:21:01 +0600

    добавлены: Конструктор, метод Передвинуть, и переписан метод ToString()

--- OOP_Basics_project_Snake/OOP_Basics_project_Snake/Point.cs
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Point.cs
@@ -14,11 +14,26 @@ namespace OOP_Basics_project_Snake
         {
         }
 
+        public Point(Point pointToCopyFrom)
+        {
+            x = pointToCopyFrom.x;
+            y = pointToCopyFrom.y;
+            symb = pointToCopyFrom.symb;
+        }
+
         public Point(int x, int y, char symb)
         {
             this.x = x;
             this.y = y;
             this.symb = symb;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.LEFT)        { x -= offset; }
+            else if (direction == Direction.RIGHT)  { x += offset; }
+            else if (direction == Direction.UP)     { y -= offset; }
+            else if (direction == Direction.DOWN)   { y += offset; }
         }
 
         public bool Draw()
@@ -31,6 +46,11 @@ namespace OOP_Basics_project_Snake
                 Console.Write(symb);
                 return true;
             }
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + symb;
         }
     }
 }
