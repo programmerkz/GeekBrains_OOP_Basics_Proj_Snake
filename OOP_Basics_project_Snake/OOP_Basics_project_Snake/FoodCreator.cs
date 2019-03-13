88f1e01468e20686c1e0d7bfc79257002f01ea55
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-13 13:25:57 +0600

    добавлена генерация Еды

---
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/FoodCreator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Basics_project_Snake
+{
+    class FoodCreator
+    {
+        int mapWidth;
+        int mapHeight;
+        int mapTop;
+        int mapLeft;
+        char foodSymbol;
+
+        Random random = new Random();
+
+        public FoodCreator(int mapWidth, int mapHeight, int mapTop, int mapLeft, char foodSymbol)
+        {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            this.mapTop = mapTop;
+            this.mapLeft = mapLeft;
+            this.foodSymbol = foodSymbol;
+        }
+
+        public Point CreateFood()
+        {
+            int x = random.Next(mapLeft + 1, mapLeft + mapWidth - 1);
+            int y = random.Next(mapTop + 1, mapTop + mapHeight - 1);
+
+            return new Point(x, y, foodSymbol);
+        }
+    }
+}
