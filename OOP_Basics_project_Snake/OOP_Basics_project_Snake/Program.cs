e822047e66082f7e48b9e01d3568c5fc173bde7c
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-13 16:28:51 +0600

    начальная длина змейки установлена 3

--- OOP_Basics_project_Snake/OOP_Basics_project_Snake/Program.cs
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Program.cs
@@ -12,7 +12,7 @@ namespace OOP_Basics_project_Snake
         {
             List<Figure> gameAreaWalls = Tools.DrawGameArea(true);
 
-            Snake snake = new Snake(new Point(13, 14, Tools.SNAKE_CHAR), 13, Direction.RIGHT);
+            Snake snake = new Snake(new Point(13, 14, Tools.SNAKE_CHAR), 3, Direction.RIGHT);
             snake.Draw();
 
             FoodCreator foodCreator = new FoodCreator(Tools.GAME_AREA_WIDTH, Tools.GAME_AREA_HEIGHT, Tools.GAME_AREA_TOP, Tools.GAME_AREA_LEFT, Tools.FOOD_CHAR);
