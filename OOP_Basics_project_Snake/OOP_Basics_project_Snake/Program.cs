02b2da3cc82c16e31eb9aeaccef3cff5a9578900
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-13 11:05:37 +0600

    добавлены Потоки, протестировано движение змейкм

--- OOP_Basics_project_Snake/OOP_Basics_project_Snake/Program.cs
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace OOP_Basics_project_Snake
 {
@@ -9,12 +10,19 @@ namespace OOP_Basics_project_Snake
     {
         static void Main(string[] args)
         {
-            Tools.ShowCaption();    // вывести заголовок
-            Tools.DrawGameArea();   // нарисовать игровое поле
+            Tools.DrawGameArea(true);   // нарисовать игровое поле
 
             Point snakeTail = new Point(13, 14, '*');
-            Snake snake = new Snake(snakeTail, 7, Direction.UP);
+            Snake snake = new Snake(snakeTail, 7, Direction.RIGHT);
             snake.Draw();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(300);
+                snake.Move();
+                snake.Draw();
+            }
+            
 
 
 
