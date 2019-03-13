2952b5c5a5de8c5fb799a507eb6df3c4239837e6
Alexandr Volossatov <sanya_666@mail.ru>
2019-03-13 10:18:43 +0600

    создан перечисляемя класс Направления

---
+++ OOP_Basics_project_Snake/OOP_Basics_project_Snake/Direction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Basics_project_Snake
+{
+    enum Direction
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+}
